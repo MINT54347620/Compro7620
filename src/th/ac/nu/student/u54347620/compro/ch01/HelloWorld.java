3fd27817e482ffa9a07f4b37debd667dc479debe
mint54347620 <partoocam_za1202@hotmail.com>
2012-06-25 19:55:39 +0700

    เพิ่มข้อความ Programming is Fun!!!

--- src/th/ac/nu/student/u54347620/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54347620/compro/ch01/HelloWorld.java
@@ -3,5 +3,6 @@ package th.ac.nu.student.u54347620.compro.ch01;
 public class HelloWorld{
     public static void main(String args[]){
         System.out.println("Hello World!!!");
+        System.out.println("Programming is Fun!!!");
     }
 }
